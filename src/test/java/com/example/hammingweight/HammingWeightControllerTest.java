876a5ba003c81bde9eddffed9271f4cd989363a9
aymxng <ayman.ghanem@gmx.net>
2023-06-13 12:23:42 +0200

    add calculateHammingWeightTest and Unit Tests

---
+++ src/test/java/com/example/hammingweight/HammingWeightControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.hammingweight;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class HammingWeightControllerTest {
+
+    @Test
+    public void testCalculateHammingWeight() {
+        HammingWeightController controller = new HammingWeightController();     //Arrange
+
+        int result1 = controller.calculateHammingWeight("01234");     //Act
+        Assertions.assertEquals(4, result1);                           //Assert
+
+        int result2 = controller.calculateHammingWeight("012012");
+        Assertions.assertEquals(4, result2);
+
+        int result3 = controller.calculateHammingWeight("000050000");
+        Assertions.assertEquals(1, result3);
+    }
+}
